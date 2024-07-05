160703ab4e1f4b060fd179bf0c7ad3939a0427cb
Mladen Tsvetkov <mladen.tsvetkov@gmail.com>
2024-07-05 08:25:08 +0300

    feature: bubble sort solution

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -10,14 +10,90 @@
   index's. Continue looping through until all values are in ascending order
 */
 
+// 1. compare two items in an array that are next to each other
+// 2. If they're out of order (that is, the larger one comes first in the array) swap them.
+// 3. Then move forward one index, repeat 1. and 2.
+// 4. Check if it is the end of the array
+// 5. If yes, check if in this array traversal we did some swapping
+// 6. If yes, start from the beginning of the array
+
+
 function bubbleSort(nums) {
-  // code goes here
+  let didSwap = true
+  let arrayTraversalsCount = 0
+
+  while(didSwap) {
+    didSwap = false
+    for(let i = 0; i < nums.length - arrayTraversalsCount; i++) {
+      if(nums[i] > nums[i+1]) {
+        const temp = nums[i+1]
+
+        nums[i+1] = nums[i]
+        nums[i] = temp
+        didSwap = true
+      }
+    }
+    arrayTraversalsCount++
+  }
+
+  return nums
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+test("bubble sort empty array", function () {
+  const nums = [];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([]);
+});
+test("bubble sort odd elements array", function () {
+  const nums = [5, 3, 8, 2, 6, 4, 7, 9, 1];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+});
+test("bubble sort all elements equal", function () {
+  const nums = [5, 5, 5, 5];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([5, 5, 5, 5]);
+});
+// Additional tests
+test("bubble sort single element array", function () {
+  const nums = [1];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([1]);
+});
+
+test("bubble sort array with negative numbers", function () {
+  const nums = [3, -1, 4, -1, 5, -9, 2, 6, 5];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([-9, -1, -1, 2, 3, 4, 5, 5, 6]);
+});
+
+test("bubble sort already sorted array", function () {
+  const nums = [1, 2, 3, 4, 5];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([1, 2, 3, 4, 5]);
+});
+
+test("bubble sort reverse sorted array", function () {
+  const nums = [5, 4, 3, 2, 1];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([1, 2, 3, 4, 5]);
+});
+
+test("bubble sort array with duplicates", function () {
+  const nums = [4, 2, 4, 3, 2, 1, 3, 4];
+  const sortedNums = bubbleSort(nums);
+  expect(sortedNums).toEqual([1, 2, 2, 3, 3, 4, 4, 4]);
+});
+
+test("bubble sort large array", function () {
+  const nums = Array.from({ length: 1000 }, () => Math.floor(Math.random() * 1000));
+  const sortedNums = bubbleSort([...nums]);
+  expect(sortedNums).toEqual(nums.sort((a, b) => a - b));
 });
